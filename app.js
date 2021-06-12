75e4f302849bde85ce4e54d24a676cbfe140ad02
Rene <71095615+us69er@users.noreply.github.com>
2021-06-12 13:38:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
+const http = require('http');
 const port = process.env.PORT || 3000
-var express=require("express");
-var bodyParser=require("body-parser");
-var app=express()
-app.use(bodyParser.json());
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
-app.get('/',function(req,res){
-res.set({
-	'Access-control-Allow-Origin': '*'
-	});
-return res.redirect('index.html');
-}).listen(3000)
 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
 
-console.log("server listening at port 3000");
-
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
