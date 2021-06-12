a5815667ae4a6f166970f39f4792d0f758644b05
Rene <71095615+us69er@users.noreply.github.com>
2021-06-12 13:11:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var express=require("express");
+var bodyParser=require("body-parser");
+var app=express()
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.get('/',function(req,res){
+res.set({
+	'Access-control-Allow-Origin': '*'
+	});
+return res.redirect('index.html');
+}).listen(3000)
+
+
+console.log("server listening at port 3000");
+
